ff7b931fbe496467adc4a20706a0e3fead9be7a2
macky084 <mohanssingh84@gmail.com>
2017-05-12 16:39:42 +0530

    added a new classs

---
+++ Test2/src/webDriver/Class1.java
@@ -0,0 +1,16 @@
+package webDriver;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		 String [] str = {"ram", "shyam", "kumar", "singh"};
+		 
+		 for(String str1 : str)
+		 {
+			 System.out.println(str1);
+		 }
+
+}
+}
